800787699e855559dcdf4574b3e46a867c322185
Cristobal Torres <torresalvarezcristobal@gmail.com>
2024-06-06 17:09:39 -0400

    Configuracion CORS

--- models/server.js
+++ models/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const http = require('http')
 const socketio = require('socket.io')
 const path = require('path')
+const cors = require('cors')
 
 const Sockets = require('./sockets')
 
@@ -17,6 +18,8 @@ class Server {
     }
     //Metodos
     middlewares() {
+        //Configuracion CORS
+        this.app.use( cors() )
         //Desplegar el directorio publico
         this.app.use( express.static( path.resolve( __dirname, '../public')))
     }
